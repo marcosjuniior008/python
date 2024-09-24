7d056d251dab0a4838e848f0847d466d35a4099c
marcosjuniior008 <00001118439818SP@al.educacao.sp.gov.br>
2024-09-24 09:07:37 -0300

    .

---
+++ A02_INPUT.py
@@ -0,0 +1,13 @@
+nome = "Asdrubal"
+nome = input("Digite seu nome")
+
+print(nome)
+n1 = int(input("Digite um número inteiro"))
+n2 = input("Digite outro número inteiro")
+n2 = int(n2)
+print (n1+n2)
+
+sobrenome = input("Digite seu sobrenome: ")
+idade = input("Digite sua idade: ")
+print(sobrenome + idade)
+print(f"seu nome é {nome} {sobrenome} e sua idade é {idade}")
