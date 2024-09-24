ef45cab262c137cc9457a5297c457689d29d38a4
Marcos Junior De Souza Alcantara <00001118439818sp@aluno.educacao.sp.gov.br>
2024-09-24 08:31:50 -0300

    git inicial

---
+++ A01_HELLO.py
@@ -0,0 +1,21 @@
+print("olá, mundo")
+a = 1
+b = 2
+
+print(a + b)
+
+texto = 'Esta é um frase.'
+
+print(texto + "9")
+
+a = 15.9
+
+print(a)
+
+aumento = True
+print(aumento)
+
+media = 7.3
+novaMedia = 7,9
+print(media)
+print(novaMedia)
